3d6cc51003d4c9c2758691de0d1650cd46af9e8f
Arturo Vargas Cuevas <arturo.vargas@nexelium.com.mx>
2024-11-19 18:50:11 -0600

    update SQL scripts for Modbus RT commands table.
    add instructions to populate that table

--- sql scripts/d_public.sql
+++ sql scripts/d_public.sql
@@ -2,21 +2,23 @@ CREATE SCHEMA IF NOT EXISTS public;
 SET search_path TO public;
 
 CREATE TABLE IF NOT EXISTS modbusrtu_commands (
-  model TEXT,
-  modbus_address SMALLINT,
-  register_length SMALLINT,
-  parameter TEXT,
-  unit TEXT,
-  data_type TEXT,
-  r_w TEXT,
-  data_range TEXT,
-  industry_standard TEXT,  
-  indb BOOLEAN,
-  setup BOOLEAN,
-  "timestamp" BOOLEAN,
-  read_command TEXT,
-  write_command TEXT,
-  PRIMARY KEY (model, modbus_address)
+    model TEXT,
+    modbus_address INTEGER,
+    parameter TEXT,
+    unit TEXT,
+    register_length SMALLINT,
+    data_type TEXT,
+    r_w TEXT,
+    data_range TEXT,
+    industry_standard TEXT,
+    indb BOOLEAN DEFAULT FALSE,
+    setup_read BOOLEAN DEFAULT FALSE,
+    setup_write BOOLEAN DEFAULT FALSE,
+    "timestamp" BOOLEAN DEFAULT FALSE,
+    read_command TEXT DEFAULT NULL,
+    write_command TEXT DEFAULT NULL,
+    value_weight NUMERIC DEFAULT NULL,
+    PRIMARY KEY (model, modbus_address)
 );
 
 CREATE TABLE IF NOT EXISTS supported_models (
