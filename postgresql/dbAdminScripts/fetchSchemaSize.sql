635ba29b37f11d41160038fa2c2b127f5b44e693
Arturo Vargas Cuevas <arturo.vargas@nexelium.com.mx>
2025-02-24 19:08:19 -0600

    Replace fetchDemoSchemaSize.sql with fetchSchemaSize.sql for improved schema size retrieval

--- postgresql/dbAdminScripts/fetchSchemaSize.sql
+++ postgresql/dbAdminScripts/fetchSchemaSize.sql
@@ -1,3 +1,3 @@
 SELECT pg_size_pretty(sum(pg_total_relation_size(quote_ident(schemaname) || '.' || quote_ident(tablename)))) AS schema_size
 FROM pg_tables
-WHERE schemaname = 'demo';
+WHERE schemaname = 'demo'; -- Select the schema name here
